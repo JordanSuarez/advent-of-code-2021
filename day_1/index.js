1bf4a58be56d5204bca38012daed9212c289b9ed
Jordan <suarez.jordan@orange.fr>
2021-12-01 22:21:10 +0100

    solve day 1 part 2

--- day_1/index.js
+++ day_1/index.js
@@ -1,9 +1,9 @@
 import puzzleInputs from './puzzle-inputs.js'
 
-const getIncreasedInput = () => {
+const getIncreasedInput = (inputs) => {
     let increasedInput = 0
 
-    puzzleInputs.reduce((previousValue, currentValue) => {
+    inputs.reduce((previousValue, currentValue) => {
         increasedInput += previousValue < currentValue ? 1 : 0
 
         return currentValue
@@ -12,4 +12,11 @@ const getIncreasedInput = () => {
     return increasedInput
 }
 
-console.log(getIncreasedInput())
+const measurements = () => {
+    return puzzleInputs.map((input, index) =>
+        input + puzzleInputs[index + 1] + puzzleInputs[index + 2]
+    )
+}
+
+console.log('First part result =', getIncreasedInput(puzzleInputs))
+console.log('Second part result =', getIncreasedInput(measurements()))
