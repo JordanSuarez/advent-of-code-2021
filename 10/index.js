edf1248aaa05c6a7a14b7aadb9c8991376f75fbb
Jordan <suarez.jordan@orange.fr>
2021-12-11 16:32:00 +0100

    day 10 part 2

--- 10/index.js
+++ 10/index.js
@@ -4,11 +4,18 @@ const data = fs.readFileSync('./data.txt', 'utf-8')
     .split(/\n/)
     .map(v => v.split(''))
 
-const point = {
+const corruptedPoint = {
     ')': 3,
     ']': 57,
     '}': 1197,
     '>': 25137,
+}
+
+const unclosedPoint = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4,
 }
 
 const signPair = {
@@ -18,8 +25,14 @@ const signPair = {
     '<': '>',
 }
 
-function calcResult(arr) {
-    return arr.reduce((acc, curr) => acc + point[curr], 0)
+function calcFirstResult(arr) {
+    return arr.reduce((acc, curr) => acc + corruptedPoint[curr], 0)
+}
+
+function calcSecondResult(arr) {
+    const toto = arr.map(v => v.reduce((acc, curr) => acc * 5 + unclosedPoint[curr], 0)).sort((a, b) => a - b)
+
+    return toto[Math.floor(toto.length / 2)]
 }
 
 function findCorrupted(signs) {
@@ -29,7 +42,7 @@ function findCorrupted(signs) {
             lastSign.push(sign)
         } else {
             if (signPair[lastSign[lastSign.length - 1]] === sign) {
-                lastSign.splice(lastSign.length - 1, 1)
+                lastSign.pop()
             } else {
                 return sign
             }
@@ -39,17 +52,39 @@ function findCorrupted(signs) {
     return false
 }
 
-function result () {
-    const corrupteds = []
-    for (let line of data) {
-        const corrupted = findCorrupted(line)
+function findUnclosed(signs) {
+    let leftSigns = []
+    for (let sign of signs) {
+        if (!signPair[sign]) {
+            const last = leftSigns.pop()
 
-        if (corrupted) {
-            corrupteds.push(corrupted)
+            if (signPair[last] === sign) {
+                continue
+            }
+        }
+
+        leftSigns.push(sign)
+    }
+
+    return leftSigns.map(v => signPair[v]).reverse()
+}
+
+function result () {
+    const corrupted = []
+    const unclosed = []
+
+    for (let line of data) {
+        if (findCorrupted(line)) {
+            corrupted.push(findCorrupted(line))
+        }
+
+        if (!findCorrupted(line)) {
+            unclosed.push(findUnclosed(line))
         }
     }
 
-    return calcResult(corrupteds)
+    console.log('First part result:', calcFirstResult(corrupted))
+    console.log('First part result:', calcSecondResult(unclosed))
 }
 
-console.log('First part result:', result())
+result()
